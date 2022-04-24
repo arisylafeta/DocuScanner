93ed893d7cd7d879005426a347865a5f2e056405
arisylafeta <ari.sylafeta@gmail.com>
2022-04-24 18:49:57 -0400

    Final changes

--- Docu/DocuScanner.cpp
+++ Docu/DocuScanner.cpp
@@ -1,18 +1,30 @@
+/*************************************************************************/
+/*** Title: Docu Scanner.cpp                                           ***/
+/*** Course: Computational Problem Solving CPET-121                    ***/
+/*** Developer: Arianit Sylafeta                                       ***/
+/*** Date: 04/24/2022                                                  ***/
+/*** Description: The program enables users to upload images           ***/
+/*** it scans the images, lays them flat, and grayscales them.         ***/
+/*** Finally the user can download the document produced               ***/
+/*************************************************************************/
 
+//Import all dependent libraries
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace cv;
 using namespace std;
 
-/////////////// Document Scanner  //////////////////////
-
+// Initialize global variables
 Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode, imgWarp, imgCrop;
 vector<Point> initialPoints, docPoints;
 float w = 420, h = 596;
 
+// Create the PreProcessing function that grayscales the image, detected the edges and dilates the image to increase sharpness.
 Mat preProcessing(Mat img)
 {
 	cvtColor(img, imgGray, COLOR_BGR2GRAY);
@@ -24,6 +36,7 @@ Mat preProcessing(Mat img)
 	return imgDil;
 }
 
+// Create a function that detects the border of the largest square inside the image.
 vector<Point> getContours(Mat image) {
 
 	vector<vector<Point>> contours;
@@ -62,15 +75,7 @@ vector<Point> getContours(Mat image) {
 	return biggest;
 }
 
-void drawPoints(vector<Point> points, Scalar color)
-{
-	for (int i = 0; i < points.size(); i++)
-	{
-		circle(imgOriginal, points[i], 10, color, FILLED);
-		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color, 4);
-	}
-}
-
+// Create a function that correctly numbers and reorders the edge numbers of the image, so the image is upright.
 vector<Point> reorder(vector<Point> points)
 {
 	vector<Point> newPoints;
@@ -90,6 +95,7 @@ vector<Point> reorder(vector<Point> points)
 	return newPoints;
 }
 
+//Warp the image bound within the points from previous function into a flat A4 shape.
 Mat getWarp(Mat img, vector<Point> points, float w, float h)
 {
 	Point2f src[4] = { points[0],points[1],points[2],points[3] };
@@ -101,11 +107,32 @@ Mat getWarp(Mat img, vector<Point> points, float w, float h)
 	return imgWarp;
 }
 
-void main() {
 
-	string path = "Resources/paper.jpg";
-	imgOriginal = imread(path);
-	
+int main() {
+
+	//Initialize local variables
+	string inputPath, fileName, directoryPath, outputPath;
+
+	//Greet user and explain program
+	cout << "/*************************************************************************/" << endl;
+	cout << "/***                          Document Scanner                         ***/" << endl;
+	cout << "/***                          ----------------                         ***/" << endl;
+	cout << "/***                                                                   ***/" << endl;
+	cout << "/***  This program's purpose is to help users transform their          ***/" << endl;
+	cout << "/***  images into pdf formatted documents in grayscale and flat.       ***/" << endl;
+	cout << "/***  Users can then download these documents if desired.              ***/" << endl;
+	cout << "/***                                                                   ***/" << endl;
+	cout << "/***  REQUIREMENTS: Please make sure that your desired document:       ***/" << endl;
+	cout << "/***  1. Fills the majority of the image                               ***/" << endl;
+	cout << "/***  2. Is upright(The program doesn't rotate the image).             ***/" << endl;
+	cout << "/***  3. Appears rectangular on the screen(Not curved!).               ***/" << endl;
+	cout << "/***  4. Is in jpg or png format                                       ***/" << endl;
+	cout << "/*************************************************************************/" << endl;
+
+	//Require path to image from user and read from it.
+	cout << endl << endl << "Please specify the address to your desired image." << endl;
+	getline(cin, inputPath);
+	imgOriginal = imread(inputPath);
 
 	// Preprpcessing - Step 1 
 	imgThre = preProcessing(imgOriginal);
@@ -124,10 +151,33 @@ void main() {
 	Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
 	imgCrop = imgWarp(roi);
 
-	imshow("Image", imgOriginal);
+	imshow("Image Before", imgOriginal);
 	//imshow("Image Dilation", imgThre);
 	//imshow("Image Warp", imgWarp);
-	imshow("Image Crop", imgCrop);
+	imshow("Image After", imgCrop);
 	waitKey(0);
 
+	//Give user option to download image.
+	cout << "Do you wanna download the scanned download the scanned document? Y/N?";
+	char answer;
+	cin >> answer;
+
+	if (answer == 'Y') {
+		cout << "Please specify a path for your document" << endl;
+		cin >> directoryPath;
+		cout << "Please specify a name for your document(with extension)" << endl;
+		cin >> fileName;
+		outputPath = directoryPath + '/' + fileName;
+		imwrite(outputPath, imgCrop);
+		cout << "/*************************************************************************/" << endl;
+		cout << "/***                  Thank You for using Docu Scanner!                ***/" << endl;
+		cout << "/*************************************************************************/" << endl;
+	}
+	else {
+		cout << "/*************************************************************************/" << endl;
+		cout << "/***                  Thank You for using Docu Scanner!                ***/" << endl;
+		cout << "/*************************************************************************/" << endl;
+		EXIT_SUCCESS;
+	}
+	return 0;
 }
